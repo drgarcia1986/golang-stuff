369e9c17801580e0368d6f75ca984aecacaf1741
Diego Garcia <drgarcia1986@gmail.com>
2016-12-08 09:40:41 -0200

    add bubblesort example (with tests :tada:)

---
+++ bubblesort/bubblesort.go
@@ -0,0 +1,28 @@
+package bubblesort
+
+func Sort(numbers []int) []int {
+	if len(numbers) <= 1 {
+		return numbers
+	}
+
+	pivotIdx := len(numbers) / 2
+	pivot := numbers[pivotIdx]
+
+	numbersWithoutPivot := append(numbers[:pivotIdx], numbers[pivotIdx+1:]...)
+
+	minors, biggers := separate(numbersWithoutPivot, pivot)
+	return append(append(Sort(minors), pivot), Sort(biggers)...)
+
+}
+
+func separate(numbers []int, pivot int) (minors, biggers []int) {
+	for _, n := range numbers {
+		if n <= pivot {
+			minors = append(minors, n)
+		} else {
+			biggers = append(biggers, n)
+		}
+	}
+
+	return minors, biggers
+}
