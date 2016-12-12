ace9afc0a4d3e2f32ae4f11ed3e5293cfc8fe926
Diego Garcia <drgarcia1986@gmail.com>
2016-12-11 23:47:21 -0200

    remove slack examples (I use this example in `slacker` project)

--- bubblesort/bubblesort_test.go
+++ bubblesort/bubblesort_test.go
@@ -1,14 +1,10 @@
-package bubblesort_test
+package bubblesort
 
-import (
-	"testing"
-
-	"github.com/drgarcia1986/golang-stuff/bubblesort"
-)
+import "testing"
 
 func TestSort(t *testing.T) {
 	expected := []int{1, 2, 3, 5, 10}
-	sorted := bubblesort.Sort([]int{3, 5, 1, 10, 2})
+	sorted := Sort([]int{3, 5, 1, 10, 2})
 	for i := range expected {
 		if expected[i] != sorted[i] {
 			t.Errorf("Expected %v, got %v", expected, sorted)
